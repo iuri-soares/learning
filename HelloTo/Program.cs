2de2e1e1c849097417d4ccc3a72951af5e180cc5
Iuri <iurisimao@hotmail.com>
2017-01-29 18:47:51 +0000

    Hello to

---
+++ HelloTo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+
+        }
+    }
+}
