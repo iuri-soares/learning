bf1292f6e56b0e9151464e12eefb3c580c456bb0
Iuri <iurisimao@hotmail.com>
2017-01-29 19:50:47 +0000

    exemplos

--- PrimitiveTypeInfo/Program.cs
+++ PrimitiveTypeInfo/Program.cs
@@ -6,9 +6,30 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-             Console.WriteLine();
+            // https://msdn.microsoft.com/en-us/library/ya5y69ds.aspx
+
+            Console.WriteLine();
             Console.WriteLine("Type information");
             Console.WriteLine();
+
+            Console.WriteLine($"The max Byte is {Byte.MaxValue}");
+            Console.WriteLine($"The min Byte int is {Byte.MinValue}");
+  
+
+            Console.WriteLine();
+            Console.WriteLine($"The max Int16 is {Int16.MaxValue}");
+            Console.WriteLine($"The min Int16 int is {Int16.MinValue}");
+            Console.WriteLine();
+            Console.WriteLine($"The max int is {int.MaxValue}");
+            Console.WriteLine($"The min int is {int.MinValue}");
+            Console.WriteLine();
+            Console.WriteLine($"The max long is {long.MaxValue}");
+            Console.WriteLine($"The min long is {long.MinValue}");
+
+
+
+
+
         }
     }
 }
